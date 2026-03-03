4f1c9774e844d24e42513baa7b795592054f8eb0
Muhammad Ubdale <99432158+Ubdale@users.noreply.github.com>
2026-03-03 13:03:48 +0500

    Fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,15 +5,13 @@ import { ShowsComponent } from './modules/shows/shows.component';
 import { FooterComponent } from './modules/footer/footer.component';
 import { CollapseComponent } from './modules/collapse/collapse.component';
 import { CardsComponent } from './modules/cards/cards.component';
-import { NewPageComponent } from './new-page/new-page.component';
 
 
 export const routes: Routes = [
-{path:'header', component:HeaderComponent},
-{path:'hero', component:HeroComponent},
-{path:'shows', component:ShowsComponent},
-{path:'footer', component:FooterComponent},
-{path:'collapse', component:CollapseComponent},
-{path:'cards', component:CardsComponent},
-{path: 'new', component: NewPageComponent}
+    { path: 'header', component: HeaderComponent },
+    { path: 'hero', component: HeroComponent },
+    { path: 'shows', component: ShowsComponent },
+    { path: 'footer', component: FooterComponent },
+    { path: 'collapse', component: CollapseComponent },
+    { path: 'cards', component: CardsComponent },
 ];
